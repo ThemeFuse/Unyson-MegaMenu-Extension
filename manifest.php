7c6202b1549af5ce2cfcaf370dcaef75a9014955
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2015-01-31 14:44:08 +0200

    manifest: update to v1.0.3

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'Mega Menu', 'fw' );
 $manifest['description'] = __( 'The Mega Menu extension adds a user-friendly drop down menu that will let you easily create highly customized menu configurations.', 'fw' );
-$manifest['version'] = '1.0.2';
+$manifest['version'] = '1.0.3';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
