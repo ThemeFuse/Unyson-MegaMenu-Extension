f1ede2d03f72a208b9305f372149b70fdc737c5b
moldcraft <moldcraft@email.com>
2015-02-19 13:54:25 +0200

    fixed font-awesome enqueue

--- static.php
+++ static.php
@@ -1,5 +1,10 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 if (!is_admin()) {
-	wp_enqueue_style('fw-font-awesome');
+	wp_enqueue_style(
+		'fw-font-awesome',
+		fw_get_framework_directory_uri('/static/libs/font-awesome/css/font-awesome.min.css'),
+		array(),
+		fw()->manifest->get_version()
+	);
 }
